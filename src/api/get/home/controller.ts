913e18f0defd2b18a3368d80fdd6fc60e4c8e8fb
henryliu <17600047539@163.com>
2022-03-21 15:08:21 +0800

    update

--- src/api/get/home/controller.ts
+++ src/api/get/home/controller.ts
@@ -5,7 +5,7 @@ import { ResponseBody } from '../../util';
 export class HomeController {
   async getHello(ctx: BaseContext): Promise<void> {
 
-    console.log(ctx);
+    console.log(ctx.headers);
 
     ctx.body = ResponseBody.success(
       'hello world',
